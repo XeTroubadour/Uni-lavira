117e964e258281460d8dd0d94032a4bec80af25e
Xesz <Z574949185@outlook.com>
2026-04-17 20:14:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/Uni-lavira',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
